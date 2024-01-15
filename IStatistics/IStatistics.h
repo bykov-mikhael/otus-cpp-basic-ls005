75cd73588eb5d8491c8d3b0c80def1dfee5387e1
bykov-mikhael <bykov-mikhael@yandex.ru>
2024-01-15 03:15:28 +0300

    15/01/2024

---
+++ IStatistics/IStatistics.h
@@ -0,0 +1,11 @@
+class IStatistics {
+public:
+	IStatistics(int count) {
+		;
+	}
+	virtual ~IStatistics() {}
+
+	virtual void update(double next) = 0;
+	virtual double eval() const = 0;
+	virtual const char * name() const = 0;
+};
